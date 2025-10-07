45221523827dcae9190b353e14bb73f8aedb8299
ASC23-SOUNDARYA-GIT-HUB <soundarya27ascendion@gmail.com>
2025-10-07 12:58:44 +0530

    Update fun-over.py

--- Python/Lab/fun-over.py
+++ Python/Lab/fun-over.py
@@ -3,8 +3,9 @@ def add(*args):
         return ' '.join(map(str, args))
     return sum(args)
 
-print(add(10, 20))         # 30
-print(add(10, 20, 30))     # 60
-print(add(10.5, 20.1))     # 30.6
-print(add("Hello", 20))    # Hello 20
+print(add(10, 20))         
+print(add(10, 20, 30))     
+print(add(10.5, 20.1))     
+print(add("Hello", 20))    
+
 
