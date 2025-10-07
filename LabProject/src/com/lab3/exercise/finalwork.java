56dc2e9f1ebe71410315d38a63b48b9beea673e4
ASC23-SOUNDARYA-GIT-HUB <soundarya27ascendion@gmail.com>
2025-10-07 12:29:44 +0530

    Add files via upload

---
+++ LabProject/src/com/lab3/exercise/finalwork.java
@@ -0,0 +1,23 @@
+package com.lab3.exercise;
+
+public class finalwork {
+//    final class A {
+//        public void show() {
+//            System.out.println("Class A method");
+//        }
+//    }
+//
+//    // This will cause a compile-time error: "Cannot inherit from final 'A'"
+//    class B extends A {
+//        public void display() {
+//            System.out.println("Class B method");
+//        }
+//    }
+//    public static void main(String[] args){
+//
+//        B obj = new B();
+//        obj.show();
+//        obj.display();
+//
+//    }
+}
